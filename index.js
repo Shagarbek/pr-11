74c7562ad0ef64fae60b4a557de4217eda04ea7a
Shagarbek <shagarbekov@gmail.com>
2026-01-21 18:28:08 +0500

    Add /version endpoint

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ app.delete('/api/items/:id', async (req, res) => {
   res.json({ message: 'Item deleted' });
 });
 
+app.get('/version', (req, res) => {
+  res.json({
+    version: "1.1",
+    updatedAt: "2026-01-21"
+  });
+});
+
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
